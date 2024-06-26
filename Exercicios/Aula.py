9e03892c9627b96fe32d7a638e6c51e756a1b87b
LuanJesusJr <luanjesus.planet@gmail.com>
2024-06-26 17:45:35 -0300

    Um passo de cada vez

---
+++ Exercicios/Aula.py
@@ -0,0 +1,69 @@
+
+#if condição:
+    # Código a ser executado se a condição for verdadeira
+#else:
+    # Código a ser executado se a condição for falsa
+
+a1 = input('Nome do Aluno 1°: ')
+
+print('\n')
+
+n1 = int(input('Digite a nota do aluno 1°: '))
+
+print('\n')
+
+if n1 >= 60:
+    print('O Aluno {} tirou a Nota {} está APROVADO'.format(a1, n1))
+
+else:
+    print('O aluno {} Tirou a Nota {} está REPROVADO'.format(a1, n1))
+
+print('---------------------------------------------------------')
+
+
+a2 = input('Nome do Aluno 2°: ')
+
+print('\n')
+
+n2 = int(input('Digite a nota do aluno 2°: '))
+
+print('\n')
+
+if n2 >= 60:
+    print('O Aluno {} tirou a Nota {} está APROVADO'.format(a1, n1))
+
+else:
+    print('O aluno {} Tirou a Nota {} está REPROVADO'.format(a1, n1))
+
+print('---------------------------------------------------------')
+
+# a1 = input('Nome do Aluno 1°: ')
+# a2 = input('Nome do aluno 2°: ')
+# a3 = input('Nome do aluno 3°: ')
+
+# n1 = int(input('Digite a nota do aluno 1°: '))
+# n2 = int(input('Digite a nota do aluno 2°: '))
+# n3 = int(input('Digite a nota do aluno 3°: '))
+
+
+# if n1 >= 60:
+#     print('O Aluno {} tirou a Nota {} está APROVADO'.format(a1, n1))
+
+# else:
+#     print('O aluno {} Tirou a Nota {} está REPROVADO'.format(a1, n1))
+
+
+# if n2 >= 60:
+#     print('O Aluno {} tirou a Nota {} está APROVADO'.format(a2, n2))
+
+# else:
+#     print('O aluno {} Tirou a Nota {} está REPROVADO'.format(a2, n2))
+
+# if n3 >= 60:
+#     print('O Aluno {} tirou a Nota {} está APROVADO'.format(a3, n3))
+
+# else:
+#     print('O aluno {} Tirou a Nota {} está REPROVADO'.format(a3, n3))
+
+
+
---
+++ Exercicios/ex004Primitivos.py
@@ -0,0 +1,15 @@
+
+# Um conversor de temperatura de C° para F°
+# Criar um programa onde receba a temperatura em C°
+# E converta de forma automatica para F°
+
+print('\n')
+print('CONVERSOR DE C° PARA FAHRENHEIT :)\n ')
+
+n1 = int(input('Digite a temperatura em C°: '))
+print('\n')
+
+s = n1 * 9 / 5 + 32
+
+print('A temperatura em Fahrenheit é: {}'.format(s ))
+print('\n')
